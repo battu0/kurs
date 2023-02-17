2f7b32a7e5ff53ba50470bd3b776449d2c86847c
Batu Ozturk <batuozturk@protonmail.com>
2023-02-17 14:00:34 +0300

    fix unhandled promise error

--- database/group-database.js
+++ database/group-database.js
@@ -3,7 +3,7 @@ const BaseDatabase = require('./base-database');
 
 class GroupDatabase extends BaseDatabase {
     async findByName(name) {
-        const objects = this.load()
+        const objects = await this.load()
         return objects.find(o => o.name === name)
     }
 };
