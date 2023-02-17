f0553c365968498b957ad39aab1a38b48c7d1ccc
Batu Ozturk <batuozturk@protonmail.com>
2023-02-17 12:49:27 +0300

    add findByName function to group database

--- database/group-database.js
+++ database/group-database.js
@@ -2,7 +2,10 @@ const Group = require("../models/group");
 const BaseDatabase = require('./base-database');
 
 class GroupDatabase extends BaseDatabase {
-
+    async findByName(name) {
+        const objects = this.load()
+        return objects.find(o => o.name === name)
+    }
 };
 
 module.exports = new GroupDatabase(Group);
