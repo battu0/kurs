f727eb959befa4a4ec16e6186c9cde361155db7f
Batu Ozturk <batuozturk@protonmail.com>
2023-02-18 16:00:08 +0300

    add delete method

--- routes/groups.js
+++ routes/groups.js
@@ -14,10 +14,14 @@ router.post('/', async (req, res) => {
     res.send(group)
 })
 
-router.get('/:studentId', async (req, res) => {
-    const group = await groupDatabase.find(req.params.studentId)
+router.get('/:groupId', async (req, res) => {
+    const group = await groupDatabase.find(req.params.groupId)
     if (!group) return res.status(404).send('Cannot find group')
     res.render('group', { group })
+})
+
+router.delete('/:groupId', async (req, res) => {
+    groupDatabase.removeBy('id', req.params.groupId)
 })
 
 module.exports = router;
