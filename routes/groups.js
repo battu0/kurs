49ec472745420b81ae4fba44cb9c218ffd95481d
Batu Ozturk <batuozturk@protonmail.com>
2023-02-18 14:06:47 +0300

    update pug templates and refactor handler

---
+++ routes/groups.js
@@ -0,0 +1,24 @@
+const groupDatabase = require('../database/group-database');
+const Group = require('../models/group');
+const flatted  = require('flatted');
+
+const router = require('express').Router()
+
+router.get('/', async (req, res) => {
+    const groups = await groupDatabase.load()
+    res.render('groups', { groups })
+})
+
+router.post('/', async (req, res) => {
+    const student = Group.create(req.body)
+    await studentDatabase.insert(student)
+    res.send(student)
+})
+
+router.get('/:studentId', async (req, res) => {
+    const group = await groupDatabase.find(req.params.studentId)
+    if (!group) return res.status(404).send('Cannot find group')
+    res.render('group', { group })
+})
+
+module.exports = router;
