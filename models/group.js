2bd117cf3365633b95790cde945b744c6f43a0be
Batu Ozturk <batuozturk@protonmail.com>
2023-02-21 11:30:14 +0300

    change cuurent group model with MongoDB model

--- models/group.js
+++ models/group.js
@@ -1,22 +1,30 @@
-const uuid = require('uuid')
-const Enrollment = require('./enrollment');
+// const uuid = require('uuid');
+// const Enrollment = require('./enrollment');
+const mongoose = require('mongoose');
 
-class Group {
-    constructor(id = uuid.v4(), name, members = [], enrollments = []) {
-        this.id = id;
-        this.name = name;
-        this.members = members;
-        this.enrollments = enrollments;
-    }
+const GroupSchema = new mongoose.Schema({
+    name: String,
+    members: [],
+    enrollments: []
+})
 
-    enroll(course, enrollmentDate) {
-        const enrollment = new Enrollment(this, course, enrollmentDate);
-        this.enrollments.push(enrollment);
-    }
+const Group = mongoose.model(GroupSchema, 'Group')
+// class Group {
+//     constructor(id = uuid.v4(), name, members = [], enrollments = []) {
+//         this.id = id;
+//         this.name = name;
+//         this.members = members;
+//         this.enrollments = enrollments;
+//     }
 
-    static create({id, name, members, enrollments}) {
-        return new Group(id, name, members, enrollments)
-    }
-}
+//     enroll(course, enrollmentDate) {
+//         const enrollment = new Enrollment(this, course, enrollmentDate);
+//         this.enrollments.push(enrollment);
+//     }
+
+//     static create({id, name, members, enrollments}) {
+//         return new Group(id, name, members, enrollments)
+//     }
+// }
 
 module.exports = Group;
