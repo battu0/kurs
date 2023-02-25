e5f8be2dba241dd9cf578cf5bbe0f52a4aeb0146
Batu Ozturk <batuozturk@protonmail.com>
2023-02-25 19:15:27 +0300

    add views and routes. refactor groups route and layout pug template

--- routes/groups.js
+++ routes/groups.js
@@ -1,45 +1,43 @@
-const groupDatabase = require('../database/group-database');
-const Group = require('../models/group');
-const flatted  = require('flatted');
-const courseDatabase = require('../database/course-database');
+const groupService = require('../service/group-service');
+const courseService = require('../service/course-service');
 
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
-    const groups = await groupDatabase.load()
+    const groups = await groupService.load()
     res.render('groups', { groups })
 })
 
 router.post('/', async (req, res) => {
-    const group = await groupDatabase.insert(req.body)
+    const group = await groupService.insert(req.body)
     res.send(group)
 })
 
 router.get('/:groupId', async (req, res) => {
-    const group = await groupDatabase.find(req.params.groupId)
+    const group = await groupService.find(req.params.groupId)
     if (!group) return res.status(404).send('Cannot find group')
     res.render('group', { group })
 })
 
 router.delete('/:groupId', async (req, res) => {
-    await groupDatabase.removeBy('_id' ,req.params.groupId)
+    await groupService.removeBy('_id' ,req.params.groupId)
     res.send('OK')
 })
 
 router.post('/:groupId/courses', async (req, res) => {
     const {courseId, enrollmentDate} = req.body
     const {groupId} = req.params
-    const course = await courseDatabase.find(courseId)
-    const group = await groupDatabase.find(groupId)
+    const course = await courseService.find(courseId)
+    const group = await groupService.find(groupId)
     group.enroll(course, enrollmentDate)
-    await groupDatabase.update(group)
+    await groupService.update(group)
     res.send('OK')
 })
 
 router.patch('/:groupId', async (req, res) => {
     const { groupId } = req.params
     const { name } = req.body
-    await groupDatabase.update(groupId, { name })
+    await groupService.update(groupId, { name })
 })
 
 module.exports = router;
