f352bdb92dfa141ebd5d107da75885871c95d8af
Batu Ozturk <batuozturk@protonmail.com>
2023-02-26 12:44:19 +0300

    change database files with services files

--- database/course-database.js
+++
@@ -1,7 +0,0 @@
-const Course = require("../models/course");
-const BaseDatabase = require("./base-database");
-
-class CourseDatabase extends BaseDatabase {
-};
-
-module.exports = new CourseDatabase(Course);
--- database/instructor-database.js
+++
@@ -1,9 +0,0 @@
-const Instructor = require("../models/instructor");
-const BaseDatabase = require("./base-database");
-
-
-class InstructorDatabase extends BaseDatabase {
-
-};
-
-module.exports = new InstructorDatabase(Instructor);
